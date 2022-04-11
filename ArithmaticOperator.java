40b1c3a6c5cee5ab2df2afa5b866cf5cf94a39fd
monalipawar-13 <98879608+monalipawar-13@users.noreply.github.com>
2022-04-11 19:50:31 +0530

    Operator Programs

---
+++ ArithmaticOperator.java
@@ -0,0 +1,24 @@
+package operator;
+
+public class ArithmaticOperator 
+{	
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		int sum=a+b;
+		int sub=a-b;
+		int mul=a*b;
+		int div=a/b;
+		
+		int rem=10%20;
+		System.out.println("Sum of a and b"+sum);
+		System.out.println("Sub of a and b"+sub);
+		System.out.println("mul of a and b"+mul);
+		System.out.println("div of a and b"+div);
+		System.out.println("rem of a and b"+rem);
+		
+	}
+
+}
---
+++ IncDecOperator.java
@@ -0,0 +1,21 @@
+package operator;
+
+public class IncDecOperator 
+{
+
+	public static void main(String[] args) 
+	{
+		int a=10;
+		int b=20;
+		
+		System.out.println("Value of a "+a);
+		System.out.println("Value of a "+b);
+		
+		System.out.println("++a   "+(++a));
+		System.out.println("b++ "+b++);
+		System.out.println("Value of b "+b);
+	
+
+	}
+
+}
