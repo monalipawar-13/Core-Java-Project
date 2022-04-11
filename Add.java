f84f8aaa90039b184633113fca0a4c258a440bc4
monalipawar-13 <98879608+monalipawar-13@users.noreply.github.com>
2022-04-11 19:48:00 +0530

    JUnit Programs

---
+++ Add.java
@@ -0,0 +1,9 @@
+package JUnit;
+
+public class Add 
+{
+	public int addition(int a, int b) 
+	{
+		return a+b;
+	}
+}
---
+++ Mul.java
@@ -0,0 +1,10 @@
+package JUnit;
+
+public class Mul 
+{
+	public int multiplication(int a,int b)
+	{
+		return a*b;
+	}
+
+}
